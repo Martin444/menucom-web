511aa6a52be5f1e93f643b539d298c2c1fb29a76
mfarel <mfarel@openpass.com.ar>
2026-05-30 22:35:50 -0300

    Change pwa debugin

--- lib/core/pwa/pwa_install_controller.dart
+++ lib/core/pwa/pwa_install_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:js_interop';
 
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:menucom_catalog/core/pwa/pwa_install_helper.dart';
 import 'package:web/web.dart';
@@ -16,32 +17,40 @@ class PwaInstallController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    debugPrint('[PWA-DEBUG] PwaInstallController.onInit');
     _checkInitialState();
     _setupListeners();
   }
 
   @override
   void onClose() {
+    debugPrint('[PWA-DEBUG] PwaInstallController.onClose');
     window.removeEventListener('pwa-install-available', _onAvailableHandler);
     window.removeEventListener('pwa-installed', _onInstalledHandler);
     super.onClose();
   }
 
   void _checkInitialState() {
+    debugPrint('[PWA-DEBUG] _checkInitialState');
     _isInstallable.value = PwaInstallHelper.isAvailable;
-    Future.delayed(const Duration(seconds: 2), () {
+    debugPrint('[PWA-DEBUG] _checkInitialState isInstallable: ${_isInstallable.value}');
+    Future.delayed(const Duration(seconds: 3), () {
       if (!_isInstallable.value) {
         _isInstallable.value = PwaInstallHelper.isAvailable;
+        debugPrint('[PWA-DEBUG] _checkInitialState delayed retry isInstallable: ${_isInstallable.value}');
       }
     });
   }
 
   void _setupListeners() {
+    debugPrint('[PWA-DEBUG] _setupListeners');
     _onAvailableHandler = ((JSAny _) {
+      debugPrint('[PWA-DEBUG] pwa-install-available handler fired in controller');
       _isInstallable.value = true;
     }).toJS;
 
     _onInstalledHandler = ((JSAny _) {
+      debugPrint('[PWA-DEBUG] pwa-installed handler fired in controller');
       _isInstallable.value = false;
     }).toJS;
 
@@ -50,7 +59,9 @@ class PwaInstallController extends GetxController {
   }
 
   Future<void> install() async {
+    debugPrint('[PWA-DEBUG] PwaInstallController.install called, isInstallable: ${_isInstallable.value}');
     final success = await PwaInstallHelper.triggerInstall();
+    debugPrint('[PWA-DEBUG] PwaInstallController.install result: $success');
     if (success) {
       _isInstallable.value = false;
     }
--- lib/core/pwa/pwa_install_helper.dart
+++ lib/core/pwa/pwa_install_helper.dart
@@ -1,5 +1,6 @@
 import 'dart:js_interop';
 
+import 'package:flutter/foundation.dart';
 import 'package:web/web.dart' show window;
 
 @JS('isPwaInstallAvailable')
@@ -10,25 +11,43 @@ external JSPromise<JSBoolean> _triggerPwaInstall();
 
 class PwaInstallHelper {
   static bool get isAvailable {
-    return _isPwaInstallAvailable();
+    final available = _isPwaInstallAvailable();
+    debugPrint('[PWA-DEBUG] PwaInstallHelper.isAvailable => $available');
+    return available;
   }
 
   static Future<bool> triggerInstall() async {
-    final result = await _triggerPwaInstall().toDart;
-    return result.toDart;
+    debugPrint('[PWA-DEBUG] PwaInstallHelper.triggerInstall called');
+    try {
+      final result = await _triggerPwaInstall().toDart;
+      final accepted = result.toDart;
+      debugPrint('[PWA-DEBUG] PwaInstallHelper.triggerInstall result: $accepted');
+      return accepted;
+    } catch (e) {
+      debugPrint('[PWA-DEBUG] PwaInstallHelper.triggerInstall error: $e');
+      return false;
+    }
   }
 
   static void onInstallAvailable(void Function() callback) {
+    debugPrint('[PWA-DEBUG] PwaInstallHelper.onInstallAvailable registering listener');
     window.addEventListener(
       'pwa-install-available',
-      ((JSAny _) => callback()).toJS,
+      ((JSAny _) {
+        debugPrint('[PWA-DEBUG] pwa-install-available event received in Dart');
+        callback();
+      }).toJS,
     );
   }
 
   static void onInstalled(void Function() callback) {
+    debugPrint('[PWA-DEBUG] PwaInstallHelper.onInstalled registering listener');
     window.addEventListener(
       'pwa-installed',
-      ((JSAny _) => callback()).toJS,
+      ((JSAny _) {
+        debugPrint('[PWA-DEBUG] pwa-installed event received in Dart');
+        callback();
+      }).toJS,
     );
   }
 }
